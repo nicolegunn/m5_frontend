cbfb0cebd50b9d8d6887a46efc8d7e517b94d88c
Nicole Gunn <nicole@clockworkproperty.co.nz>
2024-06-17 16:59:43 +1200

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
 
 function App() {
-
-
   return (
     <>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
