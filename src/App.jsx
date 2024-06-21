ade4b8e669edb37797c5f4255235d27a1c38abda
Nicole Gunn <nicole@clockworkproperty.co.nz>
2024-06-22 07:56:19 +1200

    add FindAStation page and add route into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
+import FindAStation from "./pages/FindAStation/FindAStation.jsx";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/find-a-station" element={ <FindAStation/>} />
       </Routes>
     </>
   );
---
+++ src/pages/FindAStation/FindAStation.jsx
@@ -0,0 +1,7 @@
+import styles from "./FindAStation.module.css"
+
+export default function FindAStation() {
+  return (
+      <div className={ styles.MainContainer}>FindAStation</div>
+  )
+}
