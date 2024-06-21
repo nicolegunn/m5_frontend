61bb2dc3427b92fc25d82e35df311b6b928b17f8
jpmal22 <jpmalejandro@hotmail.com>
2024-06-22 10:11:58 +1200

    changed navbar implementation from home page to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import FindAStation from "./pages/FindAStation/FindAStation.jsx";
+import NavBar from './components/NavBar/NavBar.jsx';
 
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/find-a-station" element={ <FindAStation/>} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,11 @@
 import styles from "./Home.module.css";
-import NavBar from '../../components/NavBar/NavBar';
 
 export default function Home() {
     
     return (
 
         <>
-            <NavBar />
+
         </>
     ); 
 }
