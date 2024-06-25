4cafc1b63e7330babcead9f2f46aad1ff5adb3e5
Nicole Gunn <nicole@clockworkproperty.co.nz>
2024-06-26 09:17:37 +1200

    move footer component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 import Home from "./pages/Home/Home.jsx";
 import FindAStation from "./pages/FindAStation/FindAStation.jsx";
 
@@ -9,8 +10,9 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/find-a-station" element={ <FindAStation/>} />
+        <Route path="/find-a-station" element={<FindAStation />} />
       </Routes>
+      <Footer />
     </>
   );
 }
